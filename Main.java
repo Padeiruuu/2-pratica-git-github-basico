bf8e820dc7c0f850f9a0b47f3e41f8c504113817
Gustavo Amaro <gustavamarodasilva@gmail.com>
2023-08-25 18:37:15 -0300

    agora sim

--- Main.java
+++ Main.java
@@ -1,5 +1,45 @@
 public class Main {
     public static void main(String[] args) {
+        a();
+    }
+
+    public static void a() {
+        b();
+    }
+
+    public static void b() {
+        c();
+    }
+
+    public static void c() {
+        d();
+    }
+
+    public static void d() {
+        e();
+    }
+
+    public static void e() {
+        f();
+    }
+
+    public static void f() {
+        g();
+    }
+
+    public static void g() {
+        h();
+    }
+
+    public static void h() {
+        i();
+    }
+
+    public static void i() {
+        j();
+    }
+
+    public static void j() {
         System.out.println("Hello World!");
     }
 }
